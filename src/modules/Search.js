fb7a656c49c561878abf3c282aa3e0e743d2621b
Grace Gamble <grace.h.gamble@gmail.com>
2023-05-16 11:46:40 -0400

    waiting/loading spinner icon

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -3,12 +3,15 @@ import $ from 'jquery';
 class Search {
     // 1. Describe and create/initiate object
     constructor() {
+        this.resultsDiv = $('#search-overlay__results');
         this.openButton = $('.js-search-trigger');
         this.closeButton = $('.search-overlay__close');
         this.searchOverlay = $('.search-overlay');
         this.searchField = $('#search-term');
         this.events();
         this.isOverlayOpen = false;
+        this.isSpinnerVisible = false;
+        this.previousValue;
         this.typingTimer;
     }
     // 2. Events
@@ -16,7 +19,7 @@ class Search {
         this.openButton.on('click', this.openOverlay.bind(this));
         this.closeButton.on('click', this.closeOverlay.bind(this));
         $(document).on('keydown', this.keyPressDispatcher.bind(this));
-        this.searchField.on('keydown', this.typingLogic.bind(this));
+        this.searchField.on('keyup', this.typingLogic.bind(this));
     }
     // 3. Methods
     openOverlay() {
@@ -30,7 +33,7 @@ class Search {
         this.isOverlayOpen = false;
     }
     keyPressDispatcher(e) {
-        if(e.keyCode == 83 & !this.isOverlayOpen) {
+        if(e.keyCode == 83 & !this.isOverlayOpen && !$('input, textarea').is(':focus')) {
             this.openOverlay();
         }
         if(e.keyCode == 27 & this.isOverlayOpen) {
@@ -38,10 +41,24 @@ class Search {
         }
     }
     typingLogic() {
-        clearTimeout(this.typingTimer);
-        this.typingTimer = setTimeout(function() {
-            console.log('this is a timout test');
-        }, 2000);
+        if(this.searchField.val() != this.previousValue) {
+            clearTimeout(this.typingTimer);
+            if (this.searchField.val()) {
+                if(!this.isSpinnerVisible) {
+                    this.resultsDiv.html('<div class="spinner-loader"></div>'); 
+                    this.isSpinnerVisible = true;
+                }
+                this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+            } else {
+                this.resultsDiv.html('');
+                this.isSpinnerVisible = false;
+            }
+        }
+        this.previousValue = this.searchField.val();
+    }
+    getResults() {
+        this.resultsDiv.html('Image real search results here.');
+        this.isSpinnerVisible = false;
     }
 }
 
